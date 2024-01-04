23bc34c303756f54aa23f46dff3a41dee769cd16
lvhl2 <lvhl2@outlook.com>
2024-01-04 21:38:56 +0800

    update del index.js

--- src/app.js
+++ src/app.js
@@ -1,73 +1,80 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const session = require('koa-generic-session')
-const cors = require('koa2-cors')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const session = require("koa-generic-session");
+const cors = require("koa2-cors");
 
-const index = require('./routes/index')
-const users = require('./routes/users')
-const address = require('./routes/address')
-const shop = require('./routes/shop')
-const order = require('./routes/order')
+const users = require("./routes/users");
+const address = require("./routes/address");
+const shop = require("./routes/shop");
+const order = require("./routes/order");
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
 
-
 // 跨域
-app.use(cors({
-  // origin: 'http://localhost:8080',  // 前端 origin
-  // origin: '*',
-  credentials:true
-}))
+app.use(
+  cors({
+    // origin: 'http://localhost:8080',  // 前端 origin
+    // origin: '*',
+    credentials: true,
+  })
+);
 
 // session 配置
-app.keys = ['qwi*&^*1341AKSDJF'] // 密钥
-app.use(session({
-  cookie:{
-    path: '/',
-    httpOnly: true,
-    maxAge: 24*60*60*1000
-  }
-}))
+app.keys = ["qwi*&^*1341AKSDJF"]; // 密钥
+app.use(
+  session({
+    cookie: {
+      path: "/",
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public', {
-  maxage: 24*60*60*1000
-}))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(
+  require("koa-static")(__dirname + "/public", {
+    maxage: 24 * 60 * 60 * 1000,
+  })
+);
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug",
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-// routes address
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-app.use(address.routes(), address.allowedMethods())
-app.use(shop.routes(), shop.allowedMethods())
-app.use(order.routes(), order.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
-module.exports = app
+// routes address
+app.use(users.routes(), users.allowedMethods());
+app.use(address.routes(), address.allowedMethods());
+app.use(shop.routes(), shop.allowedMethods());
+app.use(order.routes(), order.allowedMethods());
+
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
